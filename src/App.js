9a5f7b429986f182d88cd245922d73ed38c31fd5
Abdulloh <avazbekrashidov6@gmail.com>
2022-03-02 17:21:47 +0500

    added new sections and done some sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/header/header';
+import Socials from './components/socials/socials';
+
+
+//styles
+import '../src/assets/sass/main.scss';
+import '../src/App.scss'
+
+//images
+import img from '../src/assets/img/tree.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="hero">
+        <div className="hero-intro site-intro">
+          <div className="site-intro__inner container">
+            <ul className="site-intro__list">
+              <li className="site-intro__item">
+                  <a className="site-intro__link" href="">about</a>
+              </li>
+              <li className="site-intro__item">
+                  <a className="site-intro__link" href="">contact us</a>
+              </li>
+            </ul>
+            <ul className="hero-intro__list links">
+              <li className="links__item icon phone-icon"><a className="site-intro__link">+998900000250</a></li>
+              <li className="links__item icon location-icon">г. Ташкент</li>
+            </ul>
+          </div>
+        </div>
+        <Header />
+        <div className="site-hero container">
+          <div className="site-hero__wrapper">
+            <h1 className="site-hero__title">BotanikUz</h1>
+            <p className="site-hero__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In quaerat reprehenderit accusamus odio quia rerum quam eius quas officia nobis?
+            </p>
+            <Socials />
+          </div>
+          <div className="site-hero__slider slider">
+            <img className="slider__img" src={img} alt="slider img" width="500" height="800"/>
+          </div>
+        </div>
+      </div>
+      <main className="main">
+        <h1>main</h1>
+      </main>
+    </>
   );
 }
 
