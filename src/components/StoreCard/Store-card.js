008fd647ea6e172f0235768dadf8857a765f0a0d
Abdulloh <avazbekrashidov6@gmail.com>
2022-04-13 17:38:46 +0500

    store page done

--- src/components/StoreCard/Store-card.js
+++ src/components/StoreCard/Store-card.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./Store-card.scss";
+import Img1 from "../../assets/img/img1.jpeg";
+import Img4 from "../../assets/img/img6.jpeg";
+import Img2 from "../../assets/img/blog-img.jpeg";
 
 export default function StoreCard() {
   return (
-    <div>
-      Storecard
+    <div className="store-cards">
+      <div className="store-card__box store-card card-list">
+        <div className="store-card__head head-card">
+          <img
+            className="head-card__img"
+            src={Img1}
+            alt="card img"
+            width="300"
+            height="350"
+          />
+        </div>
+        <div className="store-card__body body-card">
+          <div className="body-card__head">
+            <h4 className="body-card__title">Бересклет Японский</h4>
+            <span className="body-card__price">65.00 $</span>
+          </div>
+          <p className="body-card__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+            perferendis illo autem architecto beatae voluptatibus a tempora
+            provident doloremque consequatur libero aut eum accusamus quos.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, iusto!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ipsa?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, dignissimos.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -1,12 +1,104 @@
-import React from 'react'
+import React from "react";
 import "./Store.scss";
-import HeroPage from '../../components/Hero/hero-page/hero-page'
+import HeroPage from "../../components/Hero/hero-page/hero-page";
+import { StoreCard } from "../../components";
 
 export default function Store() {
   return (
     <div>
       <HeroPage />
-      
+      <section className="store">
+        <div className="store__wrapper container">
+          <div className="store-filter">
+            <form className="store-filter__form filter-form">
+              <input
+                className="filter-form__input"
+                type="search"
+                placeholder="Search..."
+              />
+              <button className="filter-form__btn" type="submit"></button>
+            </form>
+            <div className="store-filter__category filter-category">
+              <div className="filter-category__head">
+                <h3 className="filter-category__head-title">
+                  Product categories
+                </h3>
+              </div>
+              <ul className="filter-category__list">
+                <li className="filter-category__list-item category-item">
+                  <a className="category-item__link" href="#">
+                    Dried
+                  </a>
+                  <span className="category-item__amount">7</span>
+                </li>
+                <li className="filter-category__list-item category-item">
+                  <a className="category-item__link" href="#">
+                    Dried
+                  </a>
+                  <span className="category-item__amount">7</span>
+                </li>
+                <li className="filter-category__list-item category-item">
+                  <a className="category-item__link" href="#">
+                    Dried
+                  </a>
+                  <span className="category-item__amount">7</span>
+                </li>
+                <li className="filter-category__list-item category-item">
+                  <a className="category-item__link" href="#">
+                    Dried
+                  </a>
+                  <span className="category-item__amount">7</span>
+                </li>
+              </ul>
+            </div>
+            <div className="store-filter__tags filter-tags">
+              <div className="filter-tags__head">
+                <h3 className="filter-tags__head-title">Poduct tags</h3>
+              </div>
+              <ul className="filter-tags__list">
+                <li className="filter-tags__list-item tags-item">
+                  <button className="tags-item__btn">plant</button>
+                </li>
+                <li className="filter-tags__list-item tags-item">
+                  <button className="tags-item__btn">floor</button>
+                </li>
+                <li className="filter-tags__list-item tags-item">
+                  <button className="tags-item__btn">office</button>
+                </li>
+                <li className="filter-tags__list-item tags-item">
+                  <button className="tags-item__btn">cactus</button>
+                </li>
+                <li className="filter-tags__list-item tags-item">
+                  <button className="tags-item__btn">house plant</button>
+                </li>
+                <li className="filter-tags__list-item tags-item">
+                  <button className="tags-item__btn">plant</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="store-item">
+            <div className="store-item__header">
+              <span className="store-item__text">
+                Showing 1–9 of 24 results
+              </span>
+              <button
+                className="form-store__btn btn-grid"
+                type="button"
+              ></button>
+              <button
+                className="form-store__btn btn-list"
+                type="button"
+              ></button>
+            </div>
+            <div className="store-item__main store-main">
+              <div className="store-main__wrapper">
+                <StoreCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
