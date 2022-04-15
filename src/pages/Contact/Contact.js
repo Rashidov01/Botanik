c72cc1e5bcd969429a0d2fbc9521f7bfa05b5081
Abdulloh <avazbekrashidov6@gmail.com>
2022-04-15 22:58:19 +0500

    contact also done

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,30 +1,69 @@
 import React from "react";
 import "./Contact.scss";
+import { HeroPage } from "../../components";
 import location from "../../assets/icon/location-icon.svg";
 import phone from "../../assets/icon/phone-icon.svg";
 
 export default function Contact() {
   return (
-    <section className="contact">
-      <div className="container">
-        <ul className="contact__list">
-          <li className="contact__item item-contact">
-            <img className="item-contact__icon" src={location} alt="location icon" width="25" height="25"/>
-            <h3 className="item-contact__name">address</h3>
-            <p className="item-contact__text">г. Ташкент. Яшнабадский район, махалля «Олмос»</p>
-          </li>
-          <li className="contact__item item-contact">
-            <img className="item-contact__icon" src={location} alt="location icon" width="25" height="25"/>
-            <h3 className="item-contact__name">address</h3>
-            <p className="item-contact__text">г. Ташкент. Яшнабадский район, махалля «Олмос»</p>
-          </li>
-          <li className="contact__item item-contact">
-            <img className="item-contact__icon" src={location} alt="location icon" width="25" height="25"/>
-            <h3 className="item-contact__name">address</h3>
-            <p className="item-contact__text">г. Ташкент. Яшнабадский район, махалля «Олмос»</p>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <>
+      <HeroPage />
+      <section className="contact">
+        <div className="container">
+          <ul className="contact__list">
+            <li className="contact__item item-contact">
+              <div className="item-holder">
+                <img
+                  className="item-contact__icon"
+                  src={location}
+                  alt="location icon"
+                  width="25"
+                  height="25"
+                />
+              </div>
+              <div className="item-info">
+                <h3 className="item-contact__name">address</h3>
+                <p className="item-contact__text">
+                  г. Ташкент. Яшнабадский район, махалля «Олмос»
+                </p>
+              </div>
+            </li>
+            <li className="contact__item item-contact">
+              <div className="item-holder">
+                <img
+                  className="item-contact__icon"
+                  src={location}
+                  alt="location icon"
+                  width="25"
+                  height="25"
+                />
+              </div>
+              <div className="item-info">
+                <h3 className="item-contact__name">Телефон</h3>
+                <a className="item-contact__link" href="#">
+                  +99890000250
+                </a>
+              </div>
+            </li>
+            <li className="contact__item item-contact">
+              <div className="item-holder">
+                <img
+                  className="item-contact__icon"
+                  src={location}
+                  alt="location icon"
+                  width="25"
+                  height="25"
+                />
+              </div>
+              <div className="item-info">
+                <h3 className="item-contact__name">Время работы</h3>
+                <p className="item-contact__text">Пн. - пт. 09:00 - 19:00</p>
+                <p className="item-contact__text">Сб. - вс. 10.00 - 18.00</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
   );
 }
